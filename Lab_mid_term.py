9c9d2dbfc1cb73021fdabe5f49c3f2e6c4540fbc
Shafiul-Sabbir <sabbirvai82@gmail.com>
2023-08-07 05:09:36 +0600

    update

--- Lab_mid_term.py
+++ Lab_mid_term.py
@@ -12,20 +12,24 @@ class Hall:
         
     
     def entry_show(self, show_id, movie_name, time):
-        show_info = (show_id, movie_name, time)
+        show_info = (movie_name, show_id, time)
         self._show_list.append(show_info)
         self._seats[show_id] = [[None for _ in range (self._cols)] for _ in range(self._rows)]
     
-    def book_seats(self, show_id, seats_to_book):
+    def book_seats(self, show_id, seats_to_book,name,contact):
         seats_for_show = self._seats.get(show_id)
         print("\n")
+        successful_list = []
+        flag = 0
+        
         if seats_for_show is None:
-            print(f"Show with ID {show_id} not found.")
+            print(colored(f"\t\t...... Show with ID {show_id} not found ......",'red'))
             return
         else:
             for row, col in seats_to_book:
+                seat = ()
                 if not (1 <= row <= self._rows and 1 <= col <= self._cols):
-                    text_for_invalid_seat = colored(f"Invalid seat location for : {row}{chr(col + 64)} ", 'red', attrs=['reverse','blink'])
+                    text_for_invalid_seat = colored(f"... Invalid seat location for : {row}{chr(col + 64)} ...", 'red', attrs=['reverse','blink'])
                     print(f"\t\t{text_for_invalid_seat}")
                 
                 elif seats_for_show[row - 1][col - 1] is not None:
@@ -34,29 +38,52 @@ class Hall:
                 
                 else:
                     seats_for_show[row - 1][col - 1] = "Booked"
-                    text_for_booked_successfully = colored(f"Seat booked successfully for : {row}{chr(col + 64)} ",'green')
-                    print(f"\t\t{text_for_booked_successfully}")
+                    row = row
+                    col = chr(col + 64)
+                    seat += (row,)
+                    seat += (col,)
+                    successful_list.append(seat)
+                    flag = 1
+                  
+        
+        if flag == 1:
+            print(colored("\t\t<<<<<< TICKET BOOKED SUCCESSFULLY. >>>>>>\n",'green'))
+            print(f"Name : {name}")
+            print(f"CONTACT : {contact}")
+            for show in self._show_list:
+                if show[1] == show_id:
+                    print(f"MOVIE NAME : {show[0]} \t Time : {show[2]}")
+            print("TICKETS : ",end="")
+            for ticket in successful_list:
+                print(f"{ticket[0]}{ticket[1]}",end=" ")
+            print()
+            print(f"Hall : {self._hall_no}")
+                     
                     
     def view_show_list(self):
         print("\n\t\t<<<<<< Shows Running >>>>>>\n")
         for show in self._show_list:
-            print(f"ID : {show[0]}, \t Movie : {show[1]}, \t Time : {show[2]}")
+            print(f"MOVIE NAME: {show[0]}, \t SHOW ID : {show[1]}, \t Time : {show[2]}")
     
     def view_available_seats(self,show_id):
         seats_for_show = self._seats.get(show_id)
         if seats_for_show is None:
-            id_not_found_txt = colored(f"Show With ID {show_id} Not Found.","red",attrs=['reverse', 'blink'])
-            print(f"\n\t\t{id_not_found_txt}")
+            id_not_found_txt = colored(f"\t\t... Show With ID {show_id} Not Found ...",'red',attrs=['reverse', 'blink'])
+            print(id_not_found_txt)
             return
         print(f"\n\t<<<<<< Available Seats For Movie ID of -> {show_id} >>>>>>\n")
+        for show in self._show_list:
+            if show[1] == show_id:
+                print(f"MOVIE NAME: {show[0]},\t\t Time : {show[2]}\n")
+                
         for row in range(1,self._rows + 1):
             for col in range(1, self._cols + 1):
                 if seats_for_show[row - 1][col - 1] is None:
-                    text = colored(f"{row}{chr(col + 64)}",'blue',attrs=['reverse', 'blink'])
-                    print(text, end = " ")
+                    text = colored(f"{row}{chr(col + 64)}\t",'blue',attrs=['reverse', 'blink'])
+                    print(f"{text}", end = " ")
                     # print(f"{row}{chr(col+64)}", end = " ")
                 else:
-                    text = colored('XX', 'red', attrs=['reverse', 'blink'])
+                    text = colored('XX\t', 'red', attrs=['reverse', 'blink'])
                     print(f"{text}", end = " ")
             print()
                 
@@ -74,19 +101,20 @@ class Star_Cinema:
 
 
 hall = Hall(rows=5, cols=5, hall_no=1)
-hall.entry_show(1, "Prem er Shomadhi", "12:00 PM")
-hall.entry_show(2, "Poran er Pakhi", "04:00 PM")
+hall.entry_show(1, "Prem er Shomadhi", "Aug 08 2023 06:00 PM")
+hall.entry_show(2, "Poran er Pakhi", "Aug 08 2023 09:00 PM")
 
 
 y = True
 while(y):
     print("\n")
-    print("1. View Show List.")
-    print("2. View Available Seats.")
-    print("3. Book your Seats.")
-    print("0. Quit.")
+    print("1. VIEW ALL SHOWS TODAY.")
+    print("2. VIEW AVAILABLE SEATS.")
+    print("3. BOOK TICKETS.")
+    print("0. QUIT.")
     
-    x = int(input("\nPress the key you want to access : "))
+    x = int(input("\nENTER OPTION : "))
+
     
     if x == 1:
         hall.view_show_list()
@@ -97,9 +125,11 @@ while(y):
         hall.view_available_seats(movie_id)
         
     elif x == 3:
-        inp_movie_id = int(input("Enter Movie Id : "))
+        customer_name = input("ENTER YOUR NAME : ")
+        customar_contact = input("ENTER YOUR CONTACT NUMBER : ")
+        inp_movie_id = int(input("Enter SHOW Id : "))
         list = []
-        seats_quantity = int(input("How Much Seats Do You Need : "))
+        seats_quantity = int(input("ENTER NUMBER OF TICKETS : "))
         for i in range(seats_quantity):
             tuple = ()
             print(f"Enter Seat Position For Seat Holder Number {i + 1} :")
@@ -109,7 +139,7 @@ while(y):
             tuple += (y,)
             list.append(tuple)
             
-        hall.book_seats(inp_movie_id,list)
+        hall.book_seats(inp_movie_id,list,customer_name,customar_contact)
         
     elif x==0 :
         print("\n \t \t****** Thanks for visiting our site. ******\n")
